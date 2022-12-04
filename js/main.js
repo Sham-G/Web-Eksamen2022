eb6c0b5726eb4d57b614e35baac38a3419964ad5
Sham-G <112703359+Sham-G@users.noreply.github.com>
2022-12-04 23:52:50 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+
+$(window).on("load",function(){
+let slideIndex = $(".slide.active").index();
+const slideLen = $(".slide").length;
+
+function slideShow(){
+    console.log(slideIndex)
+    $(".slide").removeClass("active").eq(slideIndex)
+    .addClass("active");
+if(slideIndex == slideLen-1){
+slideIndex = 0;
+}
+ else{
+slideIndex++;
+
+ }
+setTimeout(slideShow,3000);
+}
+slideShow()
+})   
